703dd472ad9833fe83fd7844446e68cd1676b1ea
sureshcarpenter <sureshc38@gmail.com>
2023-03-19 12:33:27 +0530

    Initial commit

---
+++ skshop/store/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+from store.controller import authview, cart
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('collections/', views.collections, name='collections'),
+    path('collections/<str:slug>', views.collectionsview, name="collectionsview"),
+    path('collections/<str:cate_slug>/<str:prod_slug>', views.productview, name='productview'),
+    path('register/', authview.register, name="register"),
+    path('login/', authview.loginpage, name='loginpage'),
+    path('logout/', authview.logoutpage, name="logout"),
+    path('add-to-cart/', cart.addtocart, name="addtocart"),
+    path('cart/', cart.viewcart, name="cart"),
+    path('update-cart', cart.updatecart, name="updatecart"),
+    path('delete-cart-item', cart.deletecartitem, name="deletecartitem"),
+]
