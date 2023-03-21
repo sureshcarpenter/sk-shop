968e0205db902c677bbaf8b9cf61e795db7ce408
sureshcarpenter <sureshc38@gmail.com>
2023-03-21 22:05:34 +0530

    Create Place Order Model and functions

--- skshop/store/urls.py
+++ skshop/store/urls.py
@@ -18,4 +18,5 @@ urlpatterns = [
     path('add-to-wishlist', wishlist.addtowishlist, name="addtowishlist"),
     path('delete-wishlist-item', wishlist.deletewishlistitem, name="deletewishlistitem"),
     path('checkout', checkout.index, name="checkout"),
+    path('place-order', checkout.placeorder, name="placeorder"),
 ]
